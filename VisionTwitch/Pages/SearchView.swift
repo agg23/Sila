f07873b808aa84f41a70bf1706c01a749078e379
Adam Gastineau <adam@agg.im>
2024-02-24 15:56:17 -0800

    Very dumb and rough search buttons

--- VisionTwitch/Pages/SearchView.swift
+++ VisionTwitch/Pages/SearchView.swift
@@ -11,40 +11,32 @@ import Twitch
 struct SearchView: View {
     @Environment(\.authController) private var authController
 
-    @State private var loader = DataLoader<[Twitch.Category], String>()
+    @State private var loader = DataLoader<([Twitch.Category], [Channel]), String>()
     @State private var text = ""
 
-    var body: some View {
-        let _ = Self._printChanges()
+    @State private var channelsExpanded = true
+    @State private var categoriesExpanded = true
 
+    var body: some View {
         Group {
             switch self.$loader.wrappedValue.get(task: {
-                guard let api = self.authController.status.api(), self.text.count > 0 else {
-                    return []
+                guard let api = self.authController.status.api(), !self.text.isEmpty else {
+                    return ([], [])
                 }
 
-                let text = self.text
-
-                try await Task.sleep(nanoseconds: 1 * NSEC_PER_SEC)
-
-                if Task.isCancelled {
-                    print("Cancelled in sleep")
-                    return []
-                }
-
-                print("Sending HTTP for \(text)")
-
-                let (categories, _) = try await api.searchCategories(for: text)
-                print("Results for \(text)")
-                return categories
+                async let (categories, _) = try await api.searchCategories(for: self.text)
+                async let (channels, _) = try await api.searchChannels(for: self.text)
+                return try await (categories, channels)
             }, onChange: self.text) {
             case .idle:
-                Text("No data")
-            case .loading(let data):
+                self.emptyState
+            case .loading:
                 ProgressView()
-            case .finished(let data):
-                List(data) { item in
-                    Text(item.name)
+            case .finished(let (categories, channels)):
+                if categories.isEmpty && channels.isEmpty {
+                    self.emptyState
+                } else {
+                    SearchListView(channels: channels, categories: categories)
                 }
             case .error:
                 Text("An error occurred")
@@ -52,8 +44,82 @@ struct SearchView: View {
         }
         .searchable(text: self.$text, placement: .navigationBarDrawer)
     }
+
+    @ViewBuilder
+    var emptyState: some View {
+        Text("Search for channels and categories")
+    }
+}
+
+struct SearchListView: View {
+    let channels: [Channel]
+    let categories: [Twitch.Category]
+
+    var body: some View {
+        List {
+            Section("Channels") {
+                ForEach(self.channels) { channel in
+                    SearchChannelButton(channel: channel)
+                }
+            }
+            Section("Categories") {
+                ForEach(self.categories) { category in
+                    SearchCategoryButton(category: category)
+                }
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct SearchChannelButton: View {
+    @Environment(Router.self) private var router
+
+    let channel: Channel
+
+    var body: some View {
+        Button {
+            self.router.path.append(.channel(user: .id(self.channel.id)))
+        } label: {
+            GeometryReader { geometry in
+                HStack {
+                    LoadingAsyncImage(imageUrl: URL(string: self.channel.profilePictureURL), aspectRatio: 1.0)
+                        .frame(width: geometry.size.height)
+                        .padding(.trailing, 8)
+                    Text(self.channel.name)
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct SearchCategoryButton: View {
+    @Environment(Router.self) private var router
+
+    let category: Twitch.Category
+
+    var body: some View {
+        Button {
+            self.router.path.append(.category(game: .id(self.category.id)))
+        } label: {
+            GeometryReader { geometry in
+                HStack {
+                    LoadingAsyncImage(imageUrl: URL(string: self.category.boxArtUrl), aspectRatio: 0.75)
+                        .frame(width: geometry.size.height)
+                        .padding(.trailing, 8)
+                    Text(self.category.name)
+                    Spacer()
+                }
+            }
+        }
+    }
 }
 
 #Preview {
-    SearchView()
+    NavStack {
+        SearchListView(channels: CHANNEL_LIST_MOCK(), categories: CATEGORY_LIST_MOCK().prefix(20).map({ game in
+            Category(game: game)
+        }))
+    }
 }
