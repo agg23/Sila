6e35fe69979241a3d4f4833ed63c6f627aeba3fd
Adam Gastineau <adam@agg.im>
2024-03-17 10:35:57 -0700

    Fixed CategoryList having duplicates

--- Sila/Pages/CategoryListView.swift
+++ Sila/Pages/CategoryListView.swift
@@ -10,10 +10,15 @@ import Twitch
 
 struct CategoryListView: View {
     @State private var loader = StandardDataLoader<([Game], String?)>()
+    @State private var existingIds = Set<String>()
 
     var body: some View {
         StandardScrollableDataView(loader: self.$loader) { api, user in
-            try await api.getTopGames(limit: 100)
+            let (streams, cursor) = try await api.getTopGames(limit: 100)
+
+            self.existingIds = Set(streams.map({ $0.id }))
+
+            return (streams, cursor)
         } content: { categories, _ in
             if categories.isEmpty {
                 EmptyDataView(title: "No Categories", systemImage: Icon.category, message: "categories") {
@@ -35,7 +40,12 @@ struct CategoryListView: View {
             }
 
             let newData = try await apiAndUser.0.getTopGames(limit: 100, after: originalCursor)
-            return (data.0 + newData.0, newData.1)
+
+            // Prevent duplicates from appearing, due to the list resorting while being fetched
+            let newStreams = newData.games.filter({ !self.existingIds.contains($0.id) })
+            self.existingIds.formUnion(newStreams.map({ $0.id }))
+
+            return (data.0 + newStreams, newData.cursor)
         }
     }
 }
